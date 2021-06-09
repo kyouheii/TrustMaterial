2a28437f95ccc2183272c48fddf776506df33e0f
kyouheii <ytve1052@yahoo.co.jp>
2021-06-09 18:44:06 +0900

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  
+  devise_scope :user do
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+
+end
