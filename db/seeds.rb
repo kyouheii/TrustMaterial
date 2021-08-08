4299c36e06fdc53f74f6a250c311a6fd295d29e4
kyouheii <ytve1052@yahoo.co.jp>
2021-08-09 00:17:48 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ User.create!(name: "管理者",
 User.create!(name: "白神",
   email: "sample-1@email.com",
   password: "password",
-  password_confirmation: "password"
-  )
+  password_confirmation: "password"  
+)
 
 User.create!(name: "丸川",
   email: "sample-2@email.com",
